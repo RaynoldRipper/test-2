d41ae8de5acde5b6a34c2252a0481a8ef16e762c
ReynoldRipper <Raynoldripper@gmail.com>
2019-11-20 13:23:04 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,6 @@
+var iconFav = document.querySelector('.icon-fav');
+var test = document.getElementById('descr-fav');
+
+test.addEventListener('click', function() {
+  iconFav.classList.toggle('icon-fav-active')
+});
